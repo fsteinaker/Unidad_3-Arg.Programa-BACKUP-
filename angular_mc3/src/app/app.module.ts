fc6042af215dabbbf0b8764e3e15e88a483d2fbe
fsteinaker <steinaker@gmail.com>
2022-02-13 11:42:26 -0300

    Correcciones 13-02-2022 proyecto Angular, error en JavaScript externo y en recorrer lista experiencia del data.json

--- angular_mc3/src/app/app.module.ts
+++ angular_mc3/src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { PerfilComponent } from './componentes/perfil/perfil.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
+import { FormacionComponent } from './componentes/formacion/formacion.component';
+import { HabilidadesComponent } from './componentes/habilidades/habilidades.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadoComponent,
+    PerfilComponent,
+    ExperienciaComponent,
+    FormacionComponent,
+    HabilidadesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
